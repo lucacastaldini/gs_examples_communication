db919c706af7ea00f77dde4b9d14aa0a47888114
Luca Castaldini <luca.castaldini@cta-consortium.org>
2024-08-21 14:45:47 +0200

    update consumer producer specialization

--- zeromq/include/consumer.hh
+++ zeromq/include/consumer.hh
@@ -5,25 +5,33 @@
 #include <iostream>
 
 template <typename T>
-class Consumer {
+class Consumer
+{
 public:
-    Consumer(zmq::context_t& context, const std::string& address)
-        : socket(context, ZMQ_PULL) {
+    Consumer(zmq::context_t &context, const std::string &address)
+        : socket(context, ZMQ_PULL)
+    {
         socket.connect(address);
     }
 
     // Destructor to clean up the socket
-    virtual ~Consumer() {
-        try {
+    virtual ~Consumer()
+    {
+        try
+        {
             socket.close();
-        } catch (const zmq::error_t& e) {
+        }
+        catch (const zmq::error_t &e)
+        {
             std::cerr << "Error closing socket: " << e.what() << std::endl;
         }
     }
 
-    bool consume(T& value) {
+    bool consume(T &value)
+    {
         zmq::message_t message;
-        if (socket.recv(message, zmq::recv_flags::none)) {
+        if (socket.recv(message, zmq::recv_flags::none))
+        {
             memcpy(&value, message.data(), sizeof(T));
             return true;
         }
@@ -36,37 +44,91 @@ private:
 
 // Specialization for std::vector<T>
 template <typename T>
-class Consumer<std::vector<T>> {
+class Consumer<std::vector<T>>
+{
 public:
-    Consumer(zmq::context_t& context, const std::string& address)
-        : socket(context, ZMQ_PULL) {
+    Consumer(zmq::context_t &context, const std::string &address)
+        : socket(context, ZMQ_PULL)
+    {
         socket.connect(address);
     }
 
     // Destructor to clean up the socket
-    virtual ~Consumer() {
-        try {
+    virtual ~Consumer()
+    {
+        try
+        {
             socket.close();
-        } catch (const zmq::error_t& e) {
+        }
+        catch (const zmq::error_t &e)
+        {
             std::cerr << "Error closing socket: " << e.what() << std::endl;
         }
     }
 
-    bool consume(std::vector<T>& vec) {
+    bool consume(std::vector<T> &vec)
+    {
         zmq::message_t message;
-        if (socket.recv(message, zmq::recv_flags::none)) {
-            size_t size ;
-            memcpy( &size, message.data(), sizeof(size_t));
+        if (socket.recv(message, zmq::recv_flags::none))
+        {
+            size_t size;
+            memcpy(&size, message.data(), sizeof(size_t));
 
             // std::cout << "msg size is : " << size << std::endl;
             vec.resize(size);
-            
-            memcpy( vec.data(), static_cast<char*>(message.data()) + sizeof(size_t),  size * sizeof(T));
+
+            memcpy(vec.data(), static_cast<char *>(message.data()) + sizeof(size_t), size * sizeof(T));
             return true;
         }
         return false;
         // Serialize the vector size and elements into a single buffer
+    }
+
+private:
+    zmq::socket_t socket;
+};
+
+// Specialization for std::vector<T>
+template <>
+class Consumer<std::string>
+{
+public:
+    Consumer(zmq::context_t &context, const std::string &address)
+        : socket(context, ZMQ_PULL)
+    {
+        socket.connect(address);
+    }
+
+    // Destructor to clean up the socket
+    virtual ~Consumer()
+    {
+        try
+        {
+            socket.close();
         }
+        catch (const zmq::error_t &e)
+        {
+            std::cerr << "Error closing socket: " << e.what() << std::endl;
+        }
+    }
+
+    bool consume(std::string &str)
+    {
+        zmq::message_t message;
+        if (socket.recv(message, zmq::recv_flags::none))
+        {
+            size_t size;
+            memcpy(&size, message.data(), sizeof(size_t));
+
+            // std::cout << "msg size is : " << size << std::endl;
+            str.resize(size);
+
+            memcpy(str.data(), static_cast<char *>(message.data()) + sizeof(size_t), size);
+            return true;
+        }
+        return false;
+        // Serialize the vector size and elements into a single buffer
+    }
 
 private:
     zmq::socket_t socket;
--- zeromq/include/producer.hh
+++ zeromq/include/producer.hh
@@ -5,66 +5,132 @@
 #include <iostream>
 
 template <typename T>
-class Producer {
+class Producer
+{
 public:
-    Producer(zmq::context_t& context, const std::string& address)
-        : socket(context, ZMQ_PUSH) {
+    Producer(zmq::context_t &context, const std::string &address)
+        : socket(context, ZMQ_PUSH)
+    {
         socket.bind(address);
     }
 
     // Destructor to clean up the socket
-    virtual ~Producer() {
-        try {
+    virtual ~Producer()
+    {
+        try
+        {
             socket.close();
-        } catch (const zmq::error_t& e) {
+        }
+        catch (const zmq::error_t &e)
+        {
             std::cerr << "Error closing socket: " << e.what() << std::endl;
         }
     }
 
-    void produce(const T& value) {
+    void produce(const T &value)
+    {
         zmq::message_t message(sizeof(T));
         memcpy(message.data(), &value, sizeof(T));
-        socket.send(message, zmq::send_flags::none);
-    }
 
-private:
-    zmq::socket_t socket;
-};
-
-// Specialization for std::vector<T>
-template <typename T>
-class Producer<std::vector<T>> {
-public:
-    Producer(zmq::context_t& context, const std::string& address)
-        : socket(context, ZMQ_PUSH) {
-        socket.bind(address);
-    }
-
-    // Destructor to clean up the socket
-    virtual ~Producer() {
-        try {
-            socket.close();
-        } catch (const zmq::error_t& e) {
-            std::cerr << "Error closing socket: " << e.what() << std::endl;
+        try
+        {
+            if (!socket.send(message, zmq::send_flags::none))
+            {
+                std::cerr << "Error sending message." << std::endl;
+            }
+        }
+        catch (const zmq::error_t &e)
+        {
+            std::cerr << "Error sending message: " << e.what() << std::endl;
         }
     }
 
-    void produce(const std::vector<T>& vec) {
-        // Serialize the vector size and elements into a single buffer
-        size_t size = vec.size();
-        // std::cout << "msg size is : " << size << std::endl;
-        zmq::message_t message(sizeof(size_t) + size * sizeof(T));
+    private:
+        zmq::socket_t socket;
+    };
 
-        // Copy the size and data into the message buffer
-        memcpy(message.data(), &size, sizeof(size_t));
-        memcpy(static_cast<char*>(message.data()) + sizeof(size_t), vec.data(), size * sizeof(T));
+    // Specialization for std::vector<T>
+    template <typename T>
+    class Producer<std::vector<T>>
+    {
+    public:
+        Producer(zmq::context_t &context, const std::string &address)
+            : socket(context, ZMQ_PUSH)
+        {
+            socket.bind(address);
+        }
 
-        // Send the message
-        socket.send(message, zmq::send_flags::none);
-    }
+        // Destructor to clean up the socket
+        virtual ~Producer()
+        {
+            try
+            {
+                socket.close();
+            }
+            catch (const zmq::error_t &e)
+            {
+                std::cerr << "Error closing socket: " << e.what() << std::endl;
+            }
+        }
 
-private:
-    zmq::socket_t socket;
-};
+        void produce(const std::vector<T> &vec)
+        {
+            // Serialize the vector size and elements into a single buffer
+            size_t size = vec.size();
+            // std::cout << "msg size is : " << size << std::endl;
+            zmq::message_t message(sizeof(size_t) + size * sizeof(T));
+
+            // Copy the size and data into the message buffer
+            memcpy(message.data(), &size, sizeof(size_t));
+            memcpy(static_cast<char *>(message.data()) + sizeof(size_t), vec.data(), size * sizeof(T));
+
+            // Send the message
+            socket.send(message, zmq::send_flags::none);
+        }
+
+    private:
+        zmq::socket_t socket;
+    };
+
+    // Specialization for std::string
+    template <>
+    class Producer<std::string>
+    {
+    public:
+        Producer(zmq::context_t &context, const std::string &address)
+            : socket(context, ZMQ_PUSH)
+        {
+            socket.bind(address);
+        }
+
+        virtual ~Producer()
+        {
+            try
+            {
+                socket.close();
+            }
+            catch (const zmq::error_t &e)
+            {
+                std::cerr << "Error closing socket: " << e.what() << std::endl;
+            }
+        }
+
+        void produce(const std::string &str)
+        {
+            // Serialize the string length and data into a single buffer
+            size_t size = str.size();
+            zmq::message_t message(sizeof(size_t) + size);
+
+            // Copy the size and data into the message buffer
+            memcpy(message.data(), &size, sizeof(size_t));
+            memcpy(static_cast<char *>(message.data()) + sizeof(size_t), str.data(), size);
+
+            // Send the message
+            socket.send(message, zmq::send_flags::none);
+        }
+
+    private:
+        zmq::socket_t socket;
+    };
 
 #endif // PRODUCER_HH
